eda8e62f4a8ceed4aecc641d6d8255fa9d7a8e4b
Sushil KC <sushilkc2060@gmail.com>
2026-03-09 22:01:52 +0545

    Scalar API Docs

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 
 from fastapi import FastAPI
-
+from scalar_fastapi import get_scalar_api_reference
 app = FastAPI()
 
 @app.get("/")
@@ -10,4 +10,13 @@ def get_shipment():
         "status": "In transit"
     }
 
-# Run User: fastapi dev
+
+@app.get("/scalar", include_in_schema=False)
+def get_scalar_docs():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title="Scalar API"
+    )
+# pip install scalar_fastapi
+# http://127.0.0.1:8000/docs  API Documentation 
+
